d39fb9896ebc4a1f12df58126b8bb3bb26f82159
4141 <jasiekfanarty@gmail.com>
2021-05-18 08:40:40 +0200

    BTree class and its functions

--- BTree.cpp
+++ BTree.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "BTree.h"
+
+BTree::BTree(int t) {
+    this->order = t;
+    this->root = nullptr;
+}
+
+void BTree::traverse() {
+    if (this->root != nullptr) this->root->traverse();
+}
+
+void BTree::insert(int val) {
+    if (root == nullptr) {
+        // no root
+        root = new Node(this->order, true);
+        root->keys[0] = val;
+        root->currentKeys++;
+    } else {
+        if (root->currentKeys == root->maximum_items()) {
+            Node *temp = new Node(this->order, false);
+            temp->sons[0] = root;
+            temp->split(0, root);
+            int which_son = 0;
+            if (temp->keys[0] < val) which_son++;
+            temp->sons[which_son]->insert_nfull(val);
+            root = temp;
+        } else {
+            root->insert_nfull(val);
+        }
+    }
+}
